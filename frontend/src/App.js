ad54f9b40bd2be2a4663956f2bae8af8adb984cf
Edison Hien <z5308785@ad.unsw.edu.au>
2023-06-08 02:15:22 +1000

    init: frontend folders

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Navigate,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import RootLayout from "./layouts/RootLayout";
+import AuthLayout from "./layouts/AuthLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<RootLayout />}>
+      <Route path="/" element={<AuthLayout />}>
+        <Route index element={<Navigate to="/login" />} />
+        {/* <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} /> */}
+      </Route>
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ frontend/src/layouts/RootLayout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const RootLayout = () => {
+  return <Outlet />;
+};
+export default RootLayout;
