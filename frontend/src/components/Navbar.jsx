abdce8316b958369de5c165f33abec2a8de690be
Calvin Chang <calvoc123@gmail.com>
2023-06-22 21:40:25 +1000

    feat-frontend: implement logo and app name text

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { Box } from "@mui/material";
-
+import styles from './styles/Navbar.module.css'
 const Navbar = () => {
   const navbarContainerSx = {
     height: "70px",
     backgroundColor: "white",
+    display: "flex",
+    alignItems: "center"
   };
   return (
     <>
-      <Box sx={navbarContainerSx}>NAVBAR</Box>
+      <Box sx={navbarContainerSx}>
+        <img className={styles.logo} src="/Jira-Emblem.png" alt="Otis logo"/>
+        <h2>Otis</h2>
+      </Box>
+      
     </>
   );
 };
