8cb1165993718daef5356ab57bf7e3f515e8cf49
Edison Hien <z5308785@ad.unsw.edu.au>
2023-06-23 04:42:06 +1000

    merge: frontend with backend

--- backend/src/server.py
+++ backend/src/server.py
@@ -4,6 +4,15 @@ from pydantic import BaseModel
 from src.auth.register import authRegister
 from fastapi.middleware.cors import CORSMiddleware
 
+# from src.config.firestoreUtils import auth
+
+# import json
+# import requests
+# import os
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
 db = initialiseFirestore()
 app = FastAPI()
 
@@ -26,6 +35,11 @@ class taskMaster(BaseModel):
     email: str
 
 
+class loginBody(BaseModel):
+    email: str
+    password: str
+
+
 # Given a taskMaster class (including firstName, lastName, password, and email), create a new document representing
 # the user whilst also adding a slot in the authentication section of firebase. Returns the
 # uid of the authentication
@@ -42,6 +56,27 @@ async def register(item: taskMaster):
 #     return uid
 
 
-@app.get("/auth/login", summary="Logs a user in the application")
-async def login():
-    return {"message": "BABABBAA World"}
+# @app.post("/auth/login", summary="Logs a user in the application")
+# async def login(item: loginBody):
+#     token = sign_in_with_email_and_password(item.email, item.password)
+#     print(token)
+#     return {"message": "BABABBAA World"}
+
+
+# def sign_in_with_email_and_password(email, password, return_secure_token=True):
+#     payload = json.dumps(
+#         {
+#             "email": email,
+#             "password": password,
+#             "return_secure_token": return_secure_token,
+#         }
+#     )
+
+#     FIREBASE_WEB_API_KEY = os.getenv("PRIVATE_KEY")
+#     rest_api_url = (
+#         "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword"
+#     )
+
+#     r = requests.post(rest_api_url, params={"key": FIREBASE_WEB_API_KEY}, data=payload)
+
+#     return r.json()
