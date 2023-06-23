9db654f87a7af6da42f78c6cea48d3913ed64e90
Edison Hien <z5308785@ad.unsw.edu.au>
2023-06-23 17:24:35 +1000

    init-frontend: setup app layout

--- frontend/src/layouts/AppLayout.jsx
+++ frontend/src/layouts/AppLayout.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+import { Box } from "@mui/material";
 
 const AppLayout = () => {
-  return <div>AppLayout</div>;
+  const boxSx = {
+    display: "flex",
+    height: "calc(100vh - 70px)",
+  };
+  return (
+    <Box sx={boxSx}>
+      <Sidebar />
+      <Outlet />
+    </Box>
+  );
 };
 export default AppLayout;
