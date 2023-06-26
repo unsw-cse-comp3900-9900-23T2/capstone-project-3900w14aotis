d246c577e0ad7be89e06c906917fe7ab658a8d81
Sophia <z5313330@ad.unsw.edu.au>
2023-06-27 01:15:50 +1000

    feat-frontend/create/project: Added a divider between buttons on create project page.

--- frontend/src/home/ProjectPage.jsx
+++ frontend/src/home/ProjectPage.jsx
@@ -1,47 +1,51 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Divider } from "@mui/material";
 import styles from "./styles/ProjectPage.module.css";
-import AuthButton from "../components/AuthButton";
+import Headerbar from "../components/Headerbar";
+import CustomButton from "../components/CustomButton";
 
 const ProjectPage = () => {
 
   const projectPageContainerSx = {
     width: "100%",
-    display: "flex",
-    flexDirection: "row",
+    // display: "flex",
+    // flexDirection: "column",
+    
   }
 
   const projectContainerSx = {
+    width: "100%",
+    height: "calc(100vh - 70px)",
+    display: "grid",
+    placeContent: "center",
+  }
+
+  const projectOptionSx = {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    width: "26%",
-    height: "30%",
+    // width: "100%",
+    // height: "100%",
+    width: "31.125rem",
+    height: "16.75rem",
     borderRadius: "20px",
     background: "#FFF",
     boxShadow: "0px 0px 10px 3px rgba(0, 0, 0, 0.25)",
   }
 
-  const header = {
-    background: "#2684FF"
-    // return (
-    //   <h2>Start a Project</h2>
-    // )
-  }
-
   return <>
+    
     <Box sx={projectPageContainerSx}>
-      <Box sx={header}>
-        <h1 className={styles.h1Text}>Start a project</h1>
-      </Box>
+      <Headerbar text="Start a Project"/>
       <Box sx={projectContainerSx}>
-        <AuthButton text="New Project"/>
-
-        <AuthButton text="Join Project"/>
+        <Box sx={projectOptionSx}>
+          <CustomButton text="New Project"/>
+          <Divider className={styles.divider}>Or</Divider>
+          <CustomButton text="Join Project"/>
+        </Box>
       </Box>
 
     </Box>
-    {/* <div>ProjectPage</div>; */}
   
   
   </>
