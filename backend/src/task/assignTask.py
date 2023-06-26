33f297cee7bf778f3f67c4d71736cf1fcd7b02c9
z5316859@ad.unsw.edu.au <anthony.chen2@student.unsw.edu.au>
2023-06-27 01:49:20 +1000

    something wrong with updating to an array

--- backend/src/task/assignTask.py
+++ backend/src/task/assignTask.py
@@ -1,16 +1,16 @@
-from firebase_admin import firestore
+from google.cloud import firestore
 """
 This files contains helper functions to help assign a task to any taskmaster
 """
 
 def addAssignee(projectId, taskId, userId, db):
     """
-    This function takes in the ID of the task that you want to assign to, the person you want to assign
-    to that task. This adds the task to the taskmaster's list of tasks
+    This function assigns the task to a taskmaster
 
     Args:
         projectId (str): ID for the project that the task is in 
         taskId (str): ID for the task that you want to assign someone to
+        userId (str): ID for the person you want to assign
         db: database connection
     
     
@@ -18,9 +18,31 @@ def addAssignee(projectId, taskId, userId, db):
     projectRef = db.collection('projects').document(projectId)
     taskRef = projectRef.collection('tasks').document(taskId)
 
-    taskRef.update({"assignee(s)": firestore.ArrayUnion([userId])})
+    # above this is fine
+
+    taskRef.update({"assignee(s)": firestore.ArrayUnion(["john"])})
+
+    # note: update taskmaster's task list too
 
     return userId
 
 def deleteAssignee(projectId, taskId, userId, db):
-    return
+    """
+    This function deletes an assignee from a task
+
+    Args:
+        projectId (str): ID for the project that the task is in 
+        taskId (str): ID for the task that you want to remove someone from
+        userId (str): ID for the person you want to remove
+        db: database connection
+    
+    
+    """
+    projectRef = db.collection('projects').document(projectId)
+    taskRef = projectRef.collection('tasks').document(taskId)
+
+    taskRef.update({"Assignees": firestore.ArrayRemove([userId])})
+
+    # note: update taskmaster's task list too
+
+    return userId
