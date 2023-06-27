a085f31bb4671adeaec5d8f07e0e32310d55da90
Edison Hien <z5308785@ad.unsw.edu.au>
2023-06-27 13:56:56 +1000

    bugfix-frontend: profile picture in navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,33 +1,51 @@
-import React from 'react';
-import { Box } from '@mui/material';
-import styles from './styles/Navbar.module.css';
-import ProfilePicture from './ProfilePictureDropdown';
-import CreateTask from './CreateTask';
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import styles from "./styles/Navbar.module.css";
+import ProfilePicture from "./ProfilePictureDropdown";
+import CreateTask from "./CreateTask";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { stringToObject } from "../utils/helpers";
 
 const Navbar = () => {
+  const [loggedIn, setLoggedIn] = useState(
+    stringToObject(localStorage.getItem("loggedIn"))
+  );
+
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in
+      setLoggedIn(true);
+    } else {
+      // User is signed out
+      setLoggedIn(false);
+    }
+  });
+
   const navbarContainerSx = {
-    height: '70px',
-    backgroundColor: 'white',
-    display: 'flex',
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    height: "70px",
+    backgroundColor: "white",
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
   };
 
   const logoContainerSx = {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
   };
 
   return (
     <>
       <Box sx={navbarContainerSx}>
         <Box sx={logoContainerSx}>
-          <img className={styles.logo} src='/Jira-Emblem.png' alt='Otis logo' />
+          <img className={styles.logo} src="/Jira-Emblem.png" alt="Otis logo" />
           <h2>Otis</h2>
         </Box>
-        <ProfilePicture />
+        {loggedIn && <ProfilePicture />}
+
         <CreateTask />
       </Box>
     </>
