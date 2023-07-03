85a6a90897817d22eda7eff88a27c0d67bcc64cb
Winston Liang <liang.w0831@gmail.com>
2023-07-04 01:44:44 +1000

    feat-backend: update profile complete

---
+++ backend/src/profile/update.py
@@ -0,0 +1,33 @@
+from src.config.firestoreUtils import auth
+
+
+
+def updateProfile(uid,db, item):
+    """
+    This function updates the profile of a user in firebase auth and database
+
+    Args:
+        uid (str): user ID
+        db (): database
+        item (body): item containing details of the taskMaster including
+        firstName,lastName, password, email, tasks, and projects
+
+    Returns:
+        (str): user ID
+    """
+    
+    docs = db.collection("taskmasters").where("uid", "==",uid).limit(1).stream()
+    for doc in docs:
+        doc.reference.update(
+             {
+            "firstName": item.firstName,
+            "lastName": item.lastName,
+            "email": item.email,
+            "profileImage": item.profileImage,
+            "coverProfileImage":item.coverProfileImage
+            }
+        )
+
+        
+
+    return uid
