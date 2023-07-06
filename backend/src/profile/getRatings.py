eacbea5c2ee7567d90b1310cd29398e4628a3e7e
Winston Liang <liang.w0831@gmail.com>
2023-07-06 13:10:53 +1000

    feat-backend: profile/ratings complete, got rid of nesting for loops

--- backend/src/profile/getRatings.py
+++ backend/src/profile/getRatings.py
@@ -1,6 +1,15 @@
 
 
 def userRatings(uid, db):
+    """_summary_
+
+    Args:
+        uid (str): user id
+        db (_type_): database
+
+    Returns:
+        ratingDict: Dictionary containing all ratings a user has given
+    """
     parentDocRef = db.collection("projects").get()
     ratingDict = {
         "Very Happy": 0,
@@ -14,9 +23,20 @@ def userRatings(uid, db):
         taskDocRef = db.collection("projects").document(project.id).collection("tasks").get()
         for task in taskDocRef:  
             ratingMap = task.get("Rating")
-            for key,value in ratingMap.items():
-                if uid in value:
-                    ratingDict[key] += 1
+            updateDict(ratingMap,ratingDict,uid)
             
-
     return ratingDict
+
+def updateDict(ratingMap, ratingDict,uid):
+    """updates the dictionary when user is found given one of the ratings
+
+    Args:
+        ratingMap (map): map of ratings
+        ratingDict (dict): dictionary of ratings
+        uid (str): user id
+    """
+    for key,value in ratingMap.items():
+        if uid in value:
+            ratingDict[key] += 1
+    return
+
