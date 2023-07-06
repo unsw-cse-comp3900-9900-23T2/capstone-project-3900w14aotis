fb62286db0ae2fc83d11080e57e8e85be461a13b
Winston Liang <liang.w0831@gmail.com>
2023-07-06 21:55:00 +1000

    feat-backend: changed return value of the route (Get details of a task)

--- backend/src/task/getTaskDetails.py
+++ backend/src/task/getTaskDetails.py
@@ -16,11 +16,41 @@ def getDetails(projectId, taskId, db):
     """
     projectDocRef = db.collection('projects').document(projectId)
     taskDocRef = projectDocRef.collection('tasks').document(taskId)
-
+    taskDict = {}
     doc = taskDocRef.get()
+
     if doc.exists:
-        return doc.to_dict()
+        taskDict = doc.to_dict()
     else:
         return "No document found!"
+    
+    assigneeList = taskDict.pop("Assignees")
+    assigneeDictList = []
+    for assignee in assigneeList:
+        assigneeDetail = getProfDetails(assignee,db)
+        assigneeDictList.append(assigneeDetail)
+
+    taskDict["Assignees"] = assigneeDictList
+
+    return taskDict
+
+
+
+def getProfDetails(email,db):
+    """gets user details given a user id (email, firstname, lastname,uid, tasks, projects)
+
+    Args:
+        uid (str): user id
+        db (_type_): database
+
+    Returns:
+        profDetails(dict): dictionary containing user details
+    """
+    profDetails = {}
+    docs = db.collection("taskmasters").where("email", "==",email).limit(1).stream()
+    for doc in docs:
+        profDetails = doc.to_dict()
+
+    return profDetails
 
     
