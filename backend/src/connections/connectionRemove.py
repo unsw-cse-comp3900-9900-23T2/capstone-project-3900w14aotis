cc64a181bf541ad641b4160a47ed63e3fe3b623f
z5316859@ad.unsw.edu.au <anthony.chen2@student.unsw.edu.au>
2023-07-14 18:20:22 +1000

    feat-backend/task/connectedTo : rearranged code for style

--- backend/src/connections/connectionRemove.py
+++ backend/src/connections/connectionRemove.py
@@ -5,13 +5,12 @@ from src.connections.connectionHelper import isConnectedTo
 This files contains helper functions to help send a connection to a taskmaster
 """
 def unfriend(currUser, userId, db):
-    # removes connection from currUser
-    currUserRef = findUser("uid", currUser, db)
-
     # check if users are connected
     if not isConnectedTo(currUser, "uid", userId, db):
         return "Users are not connected!"
 
+    # removes connection from currUser
+    currUserRef = findUser("uid", currUser, db)
     currUserRef.update(
         {"connectedTo": firestore.ArrayRemove([userId])}
     )
