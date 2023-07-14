53fe09e6c6684c8aba481a1e2fd5e050bf72530e
z5316859@ad.unsw.edu.au <anthony.chen2@student.unsw.edu.au>
2023-07-14 18:24:51 +1000

    feat-backend/task/connectedTo: added checking for send - checks if request has already been sent

--- backend/src/connections/sendConnection.py
+++ backend/src/connections/sendConnection.py
@@ -1,5 +1,5 @@
 from google.cloud import firestore
-from src.serverHelper import findUser
+from src.serverHelper import findUser, getUserDoc
 from src.connections.connectionHelper import isConnectedTo
 """
 This files contains helper functions to help send a connection to a taskmaster
@@ -10,6 +10,13 @@ def sendConnection(userEmail, userId, db):
     
     if isConnectedTo(userId, "email", userEmail, db):
         return "User is already connected!"
+    
+    # check if user has already sent a req to given email
+    userDoc = getUserDoc("email", lowerEmail, db)
+    pendingList = userDoc.pop("pendingConnections")
+    if userId in pendingList:
+        return "User already sent a request"
+    
 
     taskmasterRef = findUser("email", lowerEmail, db)
     
