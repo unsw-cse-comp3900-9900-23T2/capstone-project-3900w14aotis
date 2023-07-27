7b710e094d22519958082dee2264a0d8af8748cc
z5316859@ad.unsw.edu.au <anthony.chen2@student.unsw.edu.au>
2023-07-27 15:03:13 +1000

    feat-backend: taskNum weight is now variable based on mood and priority. usersTaskRating has now been modified to search through the ratingMap, taking into account the list of dictionaries in each map entry

--- backend/src/workload/calculateWorkload.py
+++ backend/src/workload/calculateWorkload.py
@@ -6,22 +6,26 @@ This file contains helper functions to calculate workload for a user.
 
 DEFAULT_WEIGHT = 10.0
 MAX_WEIGHT = 100.0
+OVERLOADED = -1
+DONE_STATUS = "Done"
 
 def calculate(currUser, db):
     #1. basic weighted task out of total tasks
     taskList = getFromUser("uid", currUser, "tasks", db)
 
-    # max num tasks is 10, this is full workload (100%)
-    taskNum = len(taskList)
-    if taskNum > 10:
-        return MAX_WEIGHT
-
+    # Initialise number of tasks and total workload to 0
     totalWorkload = 0
     totalTaskWeight = DEFAULT_WEIGHT
 
     for taskId in taskList:
-        #2. rating system adjustments
         projectId = getProjectID(taskId, db)
+
+        #1. Num of tasks (not marked as "done")
+        taskStatus = getFromTask(projectId, taskId, "Status", db)
+        if taskStatus == DONE_STATUS:
+            continue
+
+        #2. rating system adjustments
         moodRating = usersTaskRating(projectId, taskId, currUser, db)
         moodWeight = 1.0
         match moodRating:
@@ -38,7 +42,6 @@ def calculate(currUser, db):
             case _:
                 moodWeight = 1
         
-        
         #3. priority system adjustments
         taskPrio = getFromTask(projectId, taskId, "Priority", db)
         prioWeight = 1.0
@@ -52,8 +55,9 @@ def calculate(currUser, db):
 
         totalTaskWeight = DEFAULT_WEIGHT * moodWeight * prioWeight
         totalWorkload += totalTaskWeight
-    
+
+    # If workload is greater than 100%, workload is overloaded
     if totalWorkload > MAX_WEIGHT:
-        return MAX_WEIGHT
+        return OVERLOADED
 
     return totalWorkload
--- backend/src/workload/workloadHelper.py
+++ backend/src/workload/workloadHelper.py
@@ -7,8 +7,9 @@ This file contains helper functions for anything to do with workload calculation
 def usersTaskRating(projectId, taskId, currUser, db):
     ratingMap = getFromTask(projectId, taskId, "Rating", db)
     for mood, moodList in ratingMap.items():
-        if currUser in moodList:
-            return mood
+        for entry in moodList:
+            if entry["uid"] == currUser:
+                return mood
         
     # if mood not returned, return default "Neutral" mood
     return "Neutral"
